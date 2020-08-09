82ad77a041b4e459a7d5c9d03accc69b4d97bb72
Bryan Apellanes <bryan@threeheadz.com>
2020-08-09 06:41:06 -0700

    ran configure.sh script

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -2,6 +2,6 @@ using System.Reflection;
 using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.680812611")]
-[assembly: AssemblyCommit("a7f97d5")]
-[assembly: AssemblySemanticVersion("1.0.5-test+a7f97d5")]
+[assembly: AssemblyFileVersion("1.0.5.1842421051")]
+[assembly: AssemblyCommit("35e6bba")]
+[assembly: AssemblySemanticVersion("1.0.5-test+35e6bba")]
