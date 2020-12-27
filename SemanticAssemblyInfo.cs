fc50c09372ed73bc1b08fc49261ac4a455236060
Bryan Apellanes <bryan.apellanes@gmail.com>
2020-12-27 07:07:01 -0800

    update build scripts

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -3,7 +3,7 @@ using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
 [assembly: AssemblyFileVersion("1.0.5.0")]
-[assembly: AssemblyCommit("0297aeb")]
-[assembly: AssemblySemanticVersion("1.0.5-test+0297aeb")]
-[assembly: AssemblyDescription("`bake` is used to build and test the BamFramework and toolkit.  The compilation process is delegated to `dotnet`.; SemanticVersion=1.0.5-test+0297aeb, Revision=1.0.5.1340207779")]
+[assembly: AssemblyCommit("6acfa77")]
+[assembly: AssemblySemanticVersion("1.0.5-test+6acfa77")]
+[assembly: AssemblyDescription("`bake` is used to build and test the BamFramework and toolkit.  The compilation process is delegated to `dotnet`.; SemanticVersion=1.0.5-test+6acfa77, Revision=1.0.5.543977068")]
 [assembly: AssemblyProduct("bake")]
