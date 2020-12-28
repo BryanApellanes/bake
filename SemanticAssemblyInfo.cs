a19b375258e0a21834f4e39e0d4b153151dc48ab
Bryan Apellanes <bryan.apellanes@gmail.com>
2020-12-28 09:36:30 -0800

    updated build scripts: proper handle windows overrides

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -3,7 +3,7 @@ using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
 [assembly: AssemblyFileVersion("1.0.5.0")]
-[assembly: AssemblyCommit("6acfa77")]
-[assembly: AssemblySemanticVersion("1.0.5-test+6acfa77")]
-[assembly: AssemblyDescription("`bake` is used to build and test the BamFramework and toolkit.  The compilation process is delegated to `dotnet`.; SemanticVersion=1.0.5-test+6acfa77, Revision=1.0.5.543977068")]
+[assembly: AssemblyCommit("fc50c09")]
+[assembly: AssemblySemanticVersion("1.0.5-test+fc50c09")]
+[assembly: AssemblyDescription("`bake` is used to build and test the BamFramework and toolkit.  The compilation process is delegated to `dotnet`.; SemanticVersion=1.0.5-test+fc50c09, Revision=1.0.5.2146129482")]
 [assembly: AssemblyProduct("bake")]
