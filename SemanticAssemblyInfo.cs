36e3cca29e2c6164473522926113f2fb39dc50d7
Bryan Apellanes <bryan.apellanes@gmail.com>
2021-01-03 11:09:30 -0800

    refactor build scripts

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -3,7 +3,7 @@ using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
 [assembly: AssemblyFileVersion("1.0.5.0")]
-[assembly: AssemblyCommit("fc50c09")]
-[assembly: AssemblySemanticVersion("1.0.5-test+fc50c09")]
-[assembly: AssemblyDescription("`bake` is used to build and test the BamFramework and toolkit.  The compilation process is delegated to `dotnet`.; SemanticVersion=1.0.5-test+fc50c09, Revision=1.0.5.2146129482")]
+[assembly: AssemblyCommit("18854b5")]
+[assembly: AssemblySemanticVersion("1.0.5-test+18854b5")]
+[assembly: AssemblyDescription("`bake` is used to build and test the BamFramework and toolkit.  The compilation process is delegated to `dotnet`.; SemanticVersion=1.0.5-test+18854b5, Revision=1.0.5.4280935702")]
 [assembly: AssemblyProduct("bake")]
