dee23c0995bede9320c0640059236579a05d073d
Bryan Apellanes <bryan@threeheadz.com>
2021-02-19 18:51:06 -0800

    TryGetPath for BamSettings

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -3,7 +3,7 @@ using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
 [assembly: AssemblyFileVersion("1.0.5.0")]
-[assembly: AssemblyCommit("18854b5")]
-[assembly: AssemblySemanticVersion("1.0.5-test+18854b5")]
-[assembly: AssemblyDescription("`bake` is used to build and test the BamFramework and toolkit.  The compilation process is delegated to `dotnet`.; SemanticVersion=1.0.5-test+18854b5, Revision=1.0.5.4280935702")]
+[assembly: AssemblyCommit("36e3cca")]
+[assembly: AssemblySemanticVersion("1.0.5-test+36e3cca")]
+[assembly: AssemblyDescription("`bake` is used to build and test the BamFramework and toolkit.  The compilation process is delegated to `dotnet`.; SemanticVersion=1.0.5-test+36e3cca, Revision=1.0.5.2434282776")]
 [assembly: AssemblyProduct("bake")]
