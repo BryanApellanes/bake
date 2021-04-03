210fa16d38c5cbd3eeef15d5d7fa4fb20420b12d
Bryan Apellanes <bryan@threeheadz.com>
2021-04-03 10:48:37 -0700

    minor test refactor and cleanup

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -3,7 +3,7 @@ using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
 [assembly: AssemblyFileVersion("1.0.5.0")]
-[assembly: AssemblyCommit("36e3cca")]
-[assembly: AssemblySemanticVersion("1.0.5-test+36e3cca")]
-[assembly: AssemblyDescription("`bake` is used to build and test the BamFramework and toolkit.  The compilation process is delegated to `dotnet`.; SemanticVersion=1.0.5-test+36e3cca, Revision=1.0.5.2434282776")]
+[assembly: AssemblyCommit("dee23c0")]
+[assembly: AssemblySemanticVersion("1.0.5-test+dee23c0")]
+[assembly: AssemblyDescription("`bake` is used to build and test the BamFramework and toolkit.  The compilation process is delegated to `dotnet`.; SemanticVersion=1.0.5-test+dee23c0, Revision=1.0.5.1754457711")]
 [assembly: AssemblyProduct("bake")]
