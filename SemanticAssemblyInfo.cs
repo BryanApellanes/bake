32b0926a14fb760db846e3113d358b3bf3dd132d
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-06-09 09:29:02 -0700

    reran build

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -3,7 +3,7 @@ using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
 [assembly: AssemblyFileVersion("1.0.5.0")]
-[assembly: AssemblyCommit("210fa16")]
-[assembly: AssemblySemanticVersion("1.0.5-test+210fa16")]
-[assembly: AssemblyDescription("`bake` is used to build and test the BamFramework and toolkit.  The compilation process is delegated to `dotnet`.; SemanticVersion=1.0.5-test+210fa16, Revision=1.0.5.270512069")]
+[assembly: AssemblyCommit("869278f")]
+[assembly: AssemblySemanticVersion("1.0.5-test+869278f")]
+[assembly: AssemblyDescription("`bake` is used to build and test the BamFramework and toolkit.  The compilation process is delegated to `dotnet`.; SemanticVersion=1.0.5-test+869278f, Revision=1.0.5.2887302728")]
 [assembly: AssemblyProduct("bake")]
